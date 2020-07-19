96e76068ef86f3675968ec3e37d5007a63453b50
Ariel <arielbs1991@gmail.com>
2020-07-19 16:03:56 -0700

    finished styling, webpage ready

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Deluxe Burger with Cheddar, Onions and Pickles');
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian Burger with Pineapple and Teriyaki Sauce');
+INSERT INTO burgers (burger_name) VALUES ('Cali Chicken Burger with Swiss, Avocado and Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Southwestern Burger with Charred Poblano and Pepperjack Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Veggie Burger with Vegannaise, Soy Cheese and Avocado');
+INSERT INTO burgers (burger_name) VALUES ('Burger Dip with Caramelized Onions and Smoked Provolone');
